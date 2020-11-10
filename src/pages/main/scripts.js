7e1238424f3e53e317104f7e0027dc86dc7b8177
vishnyakovvv <45771541+vishnyakovvv@users.noreply.github.com>
2020-11-11 00:36:31 +0400

    добавил скрипты на скролл и модалки

---
+++ src/pages/main/scripts.js
@@ -0,0 +1,124 @@
+// top button
+(function () {
+
+    const buttonTop = document.querySelector(".btn-top__js");
+
+    if(!buttonTop) {
+        return;
+    }
+
+    function handleScroll(event) {
+
+        if (window.pageYOffset > 1500) {
+            if (buttonTop.classList.contains("visually-hidden")) {
+                buttonTop.classList.remove("visually-hidden");
+            }
+        } else {
+            if (!buttonTop.classList.contains("visually-hidden")) {
+                buttonTop.classList.add("visually-hidden");
+            }
+        }
+    }
+
+    function handleClick(event) {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("click", handleClick);
+    
+})();
+
+
+
+// sign in
+
+(function() {
+    let signIn = document.querySelector(".sign-modal_js"),
+        buttonOpen = document.querySelector(".sign-open__js"),
+        buttonClose = document.querySelector(".sign-close__js"),
+        inputText = document.querySelector(".input-text-sign_js");
+
+    buttonOpen.addEventListener("click", function() {
+        signIn.classList.add("sign_open");
+        inputText.focus();
+    });
+    buttonClose.addEventListener("click", function() {
+        signIn.classList.remove("sign_open");
+        inputText.focus();
+    });
+    window.addEventListener("keydown", function (event) {
+        if (event.code === "Escape" && signIn.classList.contains("sign_open")) {
+            signIn.classList.remove("sign_open");
+        }
+    });
+    window.addEventListener("click", function (event) {      
+        if (event.target == signIn) {
+            signIn.classList.remove("sign_open");
+        }
+    })
+})();
+
+// register
+
+(function() {
+    let register = document.querySelector(".register-modal_js"),
+        buttonOpen = document.querySelector(".register-open__js"),
+        buttonClose = document.querySelector(".register-close__js"),
+        inputText = document.querySelector(".input-text-register_js");
+
+    buttonOpen.addEventListener("click", function() {
+        register.classList.add("register_open");
+        inputText.focus();
+    });
+    buttonClose.addEventListener("click", function() {
+        register.classList.remove("register_open");
+        inputText.focus();
+    });
+    window.addEventListener("keydown", function (event) {
+        if (event.code === "Escape" && register.classList.contains("register_open")) {
+            register.classList.remove("register_open");
+        }
+    });
+    window.addEventListener("click", function (event) {      
+        if (event.target == register) {
+            register.classList.remove("register_open");
+        }
+    })
+})();
+
+// message
+
+(function() {
+    let message = document.querySelector(".message-modal_js"),
+        buttonOpen = document.querySelector(".message-open__js"),
+        buttonClose = document.querySelector(".message-close__js"),
+        inputText = document.querySelector(".input-text-message_js");
+
+        console.log(buttonOpen);
+
+    buttonOpen.addEventListener("click", function() {
+        message.classList.add("message_open");
+        inputText.focus();
+    });
+    buttonClose.addEventListener("click", function() {
+        message.classList.remove("message_open");
+        inputText.focus();
+    });
+    window.addEventListener("keydown", function (event) {
+        if (event.code === "Escape" && massage.classList.contains("message_open")) {
+            message.classList.remove("message_open");
+        }
+    });
+    window.addEventListener("click", function (event) {      
+        if (event.target == message) {
+            message.classList.remove("message_open");
+        }
+    })
+})();
+
+
+
