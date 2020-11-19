e8165e2fce1a1701cd72ecae4952c6aa3efa6d53
vishnyakovvv <45771541+vishnyakovvv@users.noreply.github.com>
2020-11-19 17:49:03 +0400

    добавил swiper

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -431,4 +431,17 @@ function getFormData(form, data = {}) {
     });
 })();
 
+(function () {
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+})();
+
 
